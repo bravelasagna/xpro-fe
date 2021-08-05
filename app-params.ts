ca6308ece14ab6f404b9f0acc3db23436162d8df
bravelasagna <galimbertidave@gmail.com>
2021-08-05 23:46:56 +0200

    1 file modified, 1 added

---
+++ app-params.ts
@@ -0,0 +1,3 @@
+export const appParams = {
+  apiURL: 'https://610c3fc166dd8f0017b76d41.mockapi.io/api/v1/'
+};
--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 
+import { appParams } from '../../app-params';
+
 export class PagesAdminUsers extends Component {
   constructor(props) {
     super(props);
+    this.state = { usersList: [], userEditId: 0 };
+    this.displayMode = 'list';
+  }
+
+  componentDidMount() {
+    this.populateUsersList();
+  }
+
+  populateUsersList() {
+    fetch(appParams.apiURL + 'User')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ usersList: responseJson });
+      });
+  }
+
+  userClicked(userId) {
+    this.displayMode = 'edit';
+    this.setState({ userEditId: userId });
   }
 
   render() {
-    return <div>manage users</div>;
+    if (this.displayMode == 'list') {
+      return this.state.usersList.map(user => (
+        <div onClick={() => this.userClicked(user.id)}>
+          User:{user.first_name + ' ' + user.last_name}
+        </div>
+      ));
+    }
+    if (this.displayMode == 'edit') {
+      return <span>this is edit for userId: {this.state.userEditId}</span>;
+    }
   }
 }
